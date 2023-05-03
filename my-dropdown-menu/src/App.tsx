58aeb6c4f3f41e84148297f70c56c8f0549d0290
Qianli Wu <qianliwu@ucla.edu>
2023-05-03 00:28:19 -0700

    added dropdown component

--- my-dropdown-menu/src/App.tsx
+++ my-dropdown-menu/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Dropdown } from './components/dropdown';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        <Dropdown />
+        
       </header>
     </div>
   );
