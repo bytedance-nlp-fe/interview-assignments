30780a8956838f9c6868935117057464ee79061c
Zeng Hailun <zengboheng01@gmail.com>
2023-05-14 02:24:45 +0800

    encapsulate menuItem component

--- intern/zillers-menu/src/components/dropDown/MenuItem.tsx
+++ intern/zillers-menu/src/components/dropDown/MenuItem.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 
-export const Menu: React.FC<{}> = () => {
+type SubMenuItem = {
+  title: string;
+  icon: string | null;
+  onClick?: () => void;
+}
+
+interface MenuItemProps {
+  title: string;
+  isCheckAble?: boolean;
+  isChecked?: boolean;
+  onClick?: () => void;
+  subMenuItems?: SubMenuItem[];
+};
+
+export const MenuItem: React.FC<MenuItemProps> = ({
+  title,
+  isCheckAble = false,
+  isChecked,
+  onClick = () => { },
+  subMenuItems,
+}) => {
   return (
     <></>
   );
--- intern/zillers-menu/src/components/dropDown/index.tsx
+++ intern/zillers-menu/src/components/dropDown/index.tsx
@@ -1,10 +1,62 @@
 import React from "react";
 import { Menu } from "./Menu";
+import { MenuItem } from "./MenuItem";
 
 export const DropDown: React.FC<{}> = () => {
+  const menuItems = [
+    {
+      title: "New Tab",
+      onClick: () => {
 
-  const actions = [];
+      }
+    },
+    {
+      title: "New Window",
+    },
+    {
+      title: "Favorite",
+      subMenuItems: [
+        {
+          icon: 'faGithub',
+          title: 'GitHub',
+        },
+        {
+          icon: 'faComment',
+          title: 'Stitches',
+        },
+        {
+          icon: 'faTwitter',
+          title: 'Twitter',
+        },
+      ],
+    },
+    {
+      title: "Downloads",
+    },
+    {
+      title: "Show Toolbar",
+      isCheckAble: true,
+      isChecked: true,
+    },
+    {
+      title: "Show Full URLs",
+      isCheckAble: true,
+      isChecked: false,
+    },
+  ];
+
   return (
-    <Menu name="options" />
+    <>
+      <Menu name="options" />
+      {
+        menuItems.map((item) => (<MenuItem
+          title={item.title}
+          isCheckAble={item.isCheckAble}
+          isChecked={item.isChecked}
+          onClick={() => { item.onClick?.(); }}
+          subMenuItems={item.subMenuItems}
+        />))
+      }
+    </>
   );
-}
+};
