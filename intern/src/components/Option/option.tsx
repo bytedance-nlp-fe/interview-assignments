341786d9170f5c0dd05eec6a960ea2379dcc43af
ChaconneLuo <asgardannila@gmail.com>
2023-06-29 02:12:49 +0800

    chore: keyboard queue

--- intern/src/components/Option/option.tsx
+++ intern/src/components/Option/option.tsx
@@ -1,15 +1,22 @@
 import './option.less';
-import React, { Dispatch, SetStateAction, createContext, forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import React, { ReactNode, forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import DropDownIcon from '../../assets/dropdown.svg?component';
+import Up from '../../assets/up.svg?component';
+import Down from '../../assets/down.svg?component';
+import Left from '../../assets/left.svg?component';
+import Right from '../../assets/right.svg?component';
+import Back from '../../assets/back.svg?component';
 import Item, { ItemProps, SubItemProps } from './item';
 export type OptionProps = {
     children: string | React.ReactNode
     items?: ItemProps[]
     openCallback?: () => void;
     hiddenQueue?: boolean
+    queueTime?: number
 }
 
 export type MenuListProps = {
+    arrowQueueRef: React.RefObject<HTMLUListElement>
     items?: ItemProps[]
 }
 
@@ -17,38 +24,56 @@ export type SubMenuProps = {
     items?: SubItemProps[]
 }
 
-// type OptionContextProps = {
-//     expand: boolean
-//     setExpand: Dispatch<SetStateAction<boolean>>
-// }
+export type ArrowQueueProps = {
+    queueTime: number
+}
 
-// const OptionContext = createContext<OptionContextProps>({} as OptionContextProps);
-// export type ArrowQueueProps = {
-// }
+const TimeOutElement = ({ queueTime, children }: { queueTime: number, children: ReactNode }) => {
+    const [show, setShow] = useState<boolean>(true);
+    useEffect(() => {
+        setTimeout(() => { setShow(false) }, queueTime)
+    });
+    if (!show) return null;
+    return <>{children}</>
+}
 
-// const ArrowQueue = forwardRef<HTMLDivElement, ArrowQueueProps>(({ }, ref) => {
-//     useImperativeHandle(ref, () => ({}));
-//     return <div className="arrow" ref={ref}></div>;
-// })
+const ArrowQueue = forwardRef<{ push: (el: ReactNode) => void }, ArrowQueueProps>(({ queueTime }, ref) => {
+    const [queue, setQueue] = useState<ReactNode[]>([]);
+    useImperativeHandle(ref, () => ({
+        push: (el: ReactNode) => setQueue([...queue, <TimeOutElement queueTime={queueTime} >{el}</TimeOutElement>]),
+    }));
+    return <ul className="arrow" ref={ref}>
+        {
+            queue.map((el, index) => <li key={index}>{el}</li>)
+        }
+    </ul>;
+})
 
-const MenuList = forwardRef<HTMLUListElement, MenuListProps>(({ items }, ref) => {
-    const [activeIndex, setActiveIndex] = useState<ItemProps['key']>(items?.length ? items[0].key : 0);
+const MenuList = forwardRef<HTMLUListElement, MenuListProps>(({ arrowQueueRef, items }, ref) => {
+    const [activeIndex] = useState<ItemProps['key']>(items?.length ? items[0].key : 0);
     const handleKeyDown = (e: React.KeyboardEvent<HTMLUListElement>) => {
         switch (e.key) {
             case 'ArrowDown':
                 console.log('down');
+                arrowQueueRef.current.push(<Down width='20px' height='20px' />);
                 break;
             case 'ArrowUp':
                 console.log('up');
+                arrowQueueRef.current.push(<Up width='20px' height='20px' />);
                 break;
             case 'ArrowLeft':
                 console.log('left');
+                arrowQueueRef.current.push(<Left width='20px' height='20px' />);
                 break;
             case 'ArrowRight':
                 console.log('right');
+                arrowQueueRef.current.push(<Right width='20px' height='20px' />);
                 break;
             case 'Enter':
                 items?.find((item) => item.key === activeIndex)?.onClick?.();
+                break;
+            case 'Escape':
+                arrowQueueRef.current.push(<Back width='20px' height='20px' />);
                 break;
             default:
                 break;
@@ -67,10 +92,10 @@ const MenuList = forwardRef<HTMLUListElement, MenuListProps>(({ items }, ref) =>
     </>
 })
 
-const Option = ({ children, openCallback, hiddenQueue, items }: OptionProps) => {
+const Option = ({ children, openCallback, hiddenQueue, items, queueTime }: OptionProps) => {
     const [open, setOpen] = useState(false);
     const buttonRef = useRef<HTMLDivElement>(null);
-    // const arrowQueueRef = useRef<ArrowQueueProps>(null);
+    const arrowQueueRef = useRef<HTMLUListElement>(null);
     const listRef = useRef<HTMLUListElement>(null);
 
     const handleListPosition = (open: boolean) => {
@@ -94,9 +119,8 @@ const Option = ({ children, openCallback, hiddenQueue, items }: OptionProps) => 
             {children}
             <DropDownIcon />
         </div>
-        <MenuList ref={listRef} items={items} />
-        {/* {hiddenQueue && <ArrowQueue />} */}
-
+        <MenuList arrowQueueRef={arrowQueueRef} ref={listRef} items={items} />
+        {!hiddenQueue && <ArrowQueue ref={arrowQueueRef} queueTime={queueTime || 1000} />}
     </>
 }
 export default Option;
