1ca25ed3c1401bb2981de86d343555c5eaad01c0
Daiming Yang <yang.daim@northeastern.edu>
2023-11-24 18:01:53 -0500

    refactor: :lipstick: use scss and classname to manage MenuList styles, for better extensibility and customizability in the future

--- intern/dropdown-button/src/components/DropdownButton/components/MenuList.tsx
+++ intern/dropdown-button/src/components/DropdownButton/components/MenuList.tsx
@@ -1,33 +1,18 @@
 import { ReactElement, ReactNode } from 'react';
+import '../styles/MenuList/MenuList.css';
 
 type MenuListProps = {
   children: ReactNode;
   isMainMenu?: boolean;
 };
 
-const defaultStyle: React.CSSProperties = {
-  position: 'absolute',
-  padding: 0,
-  margin: 0,
-  top: 0,
-  left: '100%',
-  backgroundColor: 'white',
-  borderRadius: '5px',
-};
-
-const mainMenuStyle: React.CSSProperties = {
-  ...defaultStyle,
-  paddingTop: '0.25rem',
-  top: '100%',
-  left: '50%',
-  transform: 'translateX(-50%)',
-};
-
 const MenuList: React.FC<MenuListProps> = ({
   children,
   isMainMenu,
 }: MenuListProps): ReactElement => {
-  return <ul style={isMainMenu ? mainMenuStyle : defaultStyle}>{children}</ul>;
+  return (
+    <ul className={isMainMenu ? 'menu-list-main' : 'menu-list'}>{children}</ul>
+  );
 };
 
 export default MenuList;
