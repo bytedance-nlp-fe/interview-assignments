7c4a98e9cd7356bc20d695dd48fff9f9ef14d8c3
Daiming Yang <yang.daim@northeastern.edu>
2023-11-24 01:26:39 -0500

    refactor: :art: isolate menulist and button

--- intern/dropdown-button/src/App.tsx
+++ intern/dropdown-button/src/App.tsx
@@ -1,55 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import DropdownButton from './components/DropdownButton';
-import MenuItem from './components/DropdownButton/components/MenuItem';
-import MenuList from './components/DropdownButton/components/MenuList';
-
-const button = (
-  <button
-    style={{
-      backgroundColor: '#3498db',
-      padding: '10px 20px',
-      borderRadius: '5px',
-      border: 'none',
-    }}
-  >
-    Click me
-  </button>
-);
-
-const menuList = (
-  <MenuList>
-    <MenuItem content={<>1</>} itemKey={'1'}>
-      <MenuList>
-        <MenuItem
-          content={
-            <div
-              style={{
-                width: '200px',
-                display: 'flex',
-                justifyContent: 'space-between',
-              }}
-            >
-              <i>icon</i>
-              <div>1.1</div>
-            </div>
-          }
-          itemKey={'1.1'}
-        />
-        <MenuItem content={<>1.2</>} itemKey={'1.2'} />
-        <MenuItem content={<>1.3</>} itemKey={'1.3'}>
-          <MenuList>
-            <MenuItem content={<>1.3.1</>} itemKey={'1.3.1'} />
-            <MenuItem content={<>1.3.2</>} itemKey={'1.3.2'} />
-            <MenuItem content={<>1.3.3</>} itemKey={'1.3.3'} />
-          </MenuList>
-        </MenuItem>
-      </MenuList>
-    </MenuItem>
-    <MenuItem content={<>2</>} itemKey={'2'} />
-  </MenuList>
-);
+import { button, menuList } from './demo/UserSetup';
 
 function App() {
   return (
---
+++ intern/dropdown-button/src/demo/UserSetup.tsx
@@ -0,0 +1,80 @@
+import MenuItem from '../components/DropdownButton/components/MenuItem';
+import MenuList from '../components/DropdownButton/components/MenuList';
+import {
+  RightArrowIcon,
+  StitchesIcon,
+  CheckIcon,
+  GitHubIcon,
+  TwitterIcon,
+} from './Icons';
+
+export const menuList = (
+  <MenuList>
+    <MenuItem content={'New Tab'} itemKey={'New Tab'}></MenuItem>
+    <MenuItem content={'New Window'} itemKey={'New Window'} />
+    <MenuItem
+      content={'Favorites'}
+      suffix={RightArrowIcon}
+      itemKey={'Favorites'}
+    >
+      <MenuList>
+        <MenuItem content={'GitHub'} itemKey={'GitHub'} prefix={GitHubIcon} />
+        <MenuItem
+          prefix={StitchesIcon}
+          content={'Stitches'}
+          itemKey={'Stitches'}
+        />
+        <MenuItem
+          content={'Twitter'}
+          itemKey={'Twitter'}
+          prefix={TwitterIcon}
+        />
+      </MenuList>
+    </MenuItem>
+    <MenuItem content={'Downloads'} itemKey={'Downloads'} />
+    <MenuItem
+      content={'Show Toolbar'}
+      itemKey={'Show Toolbar'}
+      prefix={CheckIcon}
+    />
+    <MenuItem content={'Show Full URLs'} itemKey={'Show Full URLs'} />
+  </MenuList>
+);
+
+export const button = (
+  <button className="button">
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+        backgroundColor: 'white',
+      }}
+    >
+      <div
+        style={{
+          lineHeight: '1.5rem',
+          verticalAlign: 'middle',
+        }}
+      >
+        Options
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          paddingLeft: '0.5rem',
+        }}
+      >
+        <svg width="12" height="12" viewBox="0 0 48 48" fill="none">
+          <path
+            d="M39.6 17.444L24.044 33 8.487 17.444"
+            stroke="#4E5969"
+            strokeWidth="2"
+          />
+        </svg>
+      </div>
+    </div>
+  </button>
+);
